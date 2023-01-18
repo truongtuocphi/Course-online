6f72743a400f71f364a60190cb287a87d7dc0845
unknown <truongphi754@gmail.com>
2023-01-19 04:49:17 +0700

    Upload drop down profiles user

--- Public/Js/style.js
+++ Public/Js/style.js
@@ -1,1 +1,14 @@
-let a = 10;
+//TODO drop profiles user when user click avatar profiles 
+let profilesUser = document.querySelector("#profiles-user");
+let boxProfilesUser = document.querySelector("#profiles-drop");
+
+profilesUser.addEventListener('click', () => {
+    boxProfilesUser.classList.toggle("show");
+});
+
+//TODO when user click outside close profiles
+window.onclick = (e) => {
+    if(!e.target.matches('#profiles-user')) {
+        boxProfilesUser.classList.remove("show");
+    }
+}
