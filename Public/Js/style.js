092aae9817fb4ea4520dac9eeb57511d5093a57a
unknown <truongphi754@gmail.com>
2023-01-20 23:16:15 +0700

    Upload Menu

--- Public/Js/style.js
+++ Public/Js/style.js
@@ -12,3 +12,37 @@ window.onclick = (e) => {
         boxProfilesUser.classList.remove("show");
     }
 }
+
+//TODO active on menu
+let menu = document.querySelectorAll(".btn-menu");
+
+for (let index = 0; index < menu.length; index++) {
+    menu[index].addEventListener('click', (e) => {
+        // xóa tất cả các class có active
+        let menuActive = document.querySelector('.active');
+        menuActive.classList.remove('active');
+        // thêm class active vào phần tử đã click 
+        menu[index].classList.add('active');
+    })
+}
+
+//TODO Collapse the menu
+let btnMenu = document.querySelector('#btn-menu');
+let hiddenElement = document.querySelectorAll('.title-menu');
+
+btnMenu.addEventListener('mouseup', (e) => {
+    let menu = document.querySelector('.list-menu');
+
+    hiddenElement.forEach((item) => {
+        item.classList.toggle('hidden');
+    });
+    menu.classList.toggle('collapse-menu');
+
+    if(menu.clientWidth == 293) {
+        let avatarUserMenu = document.querySelectorAll('.img-avt');
+        
+        for (let index = 0; index < avatarUserMenu.length; index++) {
+            avatarUserMenu[index].childNodes[1].style.width = '35px';
+        }
+    }
+});
